0583d92d6ebf11ca94511bea10d176f44af0eb8f
Victor Bo <victorbo2020@gmail.com>
2024-11-06 14:53:40 -0500

    hardcoded to find downstep position for jpmn yomitan setup

--- __init__.py
+++ __init__.py
@@ -8,7 +8,17 @@ def test_function():
     ids = mw.col.find_cards("deck:\"JP Mining\"")
     if len(ids):
         card = mw.col.get_card(ids[0])
-        showInfo(f"{card.question()}")
+        names = ""
+        for name, value in card.note().items():
+            names += name + "\n"
+        pitch_info = card.note()["PAPositions"]
+        if pitch_info:
+            downstep = pitch_info.find("[") + 14  # hardcoded to find downstep in html PAPosition field
+
+            showInfo(str(pitch_info[downstep]))
+        else:
+            showInfo("Error: no pa data")
+
 
 action = QAction("test", mw)
 qconnect(action.triggered, test_function)
